1bc05d00ea4c10c48582ebf79b3a2c5c213030d0
Serenity <limingwei12@foxmail.com>
2019-12-20 17:30:49 +0800

    添加项目文件。

---
+++ TreeViewDemo/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// <see cref="PartLMTrees" /> 这是属性的名称.
+        /// </summary>
+        public const string PartLMTreesPropertyName = "PartLMTrees";
+
+        private List<PartLMTreeData> _partLMTrees;
+
+        /// <summary>
+        /// Sets 和 gets PartLMTrees 的属性。
+        /// 对该属性值的更改将引发PropertyChanged事件。
+        /// </summary>
+        public List<PartLMTreeData> PartLMTrees
+        {
+            get
+            {
+                if (_partLMTrees == null)
+                {
+                    _partLMTrees = new List<PartLMTreeData>();
+                    _partLMTrees.Add(new PartLMTreeData("墙", new List<PartLMTreeData>() {
+                        new PartLMTreeData("基本墙01","1000"),
+                        new PartLMTreeData("基本墙02","1001"),
+                        new PartLMTreeData("基本墙03","1002"),
+                        new PartLMTreeData("基本墙04","1003")
+                    }));
+                    _partLMTrees.Add(new PartLMTreeData("窗", new List<PartLMTreeData>() {
+                        new PartLMTreeData("基本窗01","2000"),
+                        new PartLMTreeData("基本窗02","2001"),
+                        new PartLMTreeData("基本窗03","2002"),
+                        new PartLMTreeData("基本窗04","2003")
+                    }));
+                }
+                return _partLMTrees;
+            }
+
+            set
+            {
+                if (_partLMTrees == value)
+                {
+                    return;
+                }
+
+                _partLMTrees = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PartLMTreesPropertyName));
+            }
+        }
+
+        //        List<PartLMTreeData>
+        private RelayCommand<PartLMTreeData> _checkCommand;
+
+        /// <summary>
+        /// CheckCommand 事件.
+        /// </summary>
+        public RelayCommand<PartLMTreeData> CheckCommand
+        {
+            get
+            {
+                return _checkCommand
+                    ?? (_checkCommand = new RelayCommand<PartLMTreeData>(
+                    (pt) =>
+                    {
+                        if (pt.childrenList == null)
+                        {
+                            return;
+                        }
+
+                        bool isCheck = pt.isCheck;
+                        pt.childrenList.ForEach((child) => { child.isCheck = isCheck; });
+                    }));
+            }
+        }
+
+        /*private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //测试数据
+
+            List<PartLMTreeData> partLMTrees = new List<PartLMTreeData>();
+            partLMTrees.Add(new PartLMTreeData("墙", new List<PartLMTreeData>() {
+               new PartLMTreeData("基本墙01","1000"),
+               new PartLMTreeData("基本墙02","1001"),
+               new PartLMTreeData("基本墙03","1002"),
+               new PartLMTreeData("基本墙04","1003")
+            }));
+            partLMTrees.Add(new PartLMTreeData("窗", new List<PartLMTreeData>() {
+               new PartLMTreeData("基本窗01","2000"),
+               new PartLMTreeData("基本窗02","2001"),
+               new PartLMTreeData("基本窗03","2002"),
+               new PartLMTreeData("基本窗04","2003")
+            }));
+
+            this.tv_selElemList.ItemsSource = partLMTrees;
+        }
+
+        //CheckBox单击事件
+        private void tv_cbClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        //treeView Change事件
+        private void tv_selectElem(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+
+            Debug.WriteLine(sender.ToString());
+        }*/
+    }
+
+    public class PartLMTreeData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PartLMTreeData()
+        {
+        }
+
+        public PartLMTreeData(string itemName, string itemId)
+        {
+            this.itemName = itemName;
+            this.itemId = itemId;
+        }
+
+        public PartLMTreeData(string itemName, List<PartLMTreeData> childrenList)
+        {
+            this.itemName = itemName;
+            this.childrenList = childrenList;
+        }
+
+        public string itemName { get; set; }
+
+        public string itemId { get; set; }
+
+        private bool _isCheck;
+
+        public bool isCheck
+        {
+            get => _isCheck;
+            set
+            {
+                if (_isCheck.Equals(value))
+                {
+                    return;
+                }
+
+                _isCheck = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isCheck"));
+            }
+        }
+
+        public List<PartLMTreeData> childrenList { get; set; }
+    }
+}
---
+++ TreeViewDemo/RelayCommand.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace TreeViewDemo
+{
+    /// <summary>
+    /// 双向绑定中用的事件
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _execute = execute;
+
+            if (canExecute != null)
+            {
+                _canExecute = canExecute;
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            if (CanExecute(parameter) && _execute != null)
+            {
+                _execute();
+            }
+        }
+    }
+    /// <summary>
+    /// 双向绑定中用的事件
+    /// </summary>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public RelayCommand(Action<T> execute, bool keepTargetAlive = false)
+          : this(execute, (Func<T, bool>)null, keepTargetAlive)
+        {
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute, bool keepTargetAlive = false)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+            this._execute = new Action<T>(execute);
+            if (canExecute == null)
+                return;
+            this._canExecute = new Func<T, bool>(canExecute);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this._canExecute == null)
+                    return;
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (this._canExecute == null)
+                    return;
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute == null || _canExecute((T)parameter);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            if (CanExecute(parameter) && _execute != null)
+            {
+                try
+                {
+                    _execute((T)parameter);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
